0f5654e584ec06740f8835d7b91eb17ed294adf0
inafi <irfan.k.nafi@gmail.com>
2020-06-16 15:08:45 -0400

    schedule update

--- JS/schedule.js
+++ JS/schedule.js
@@ -3,5 +3,9 @@ function initialize() {
     $("body").on("click", function(){
         $(".fc-today-button").text("Today");
     })
+
+    var date = new Date();
+    console.log(date.getUTCDay());
+    $(".side .title").text($(".fc-left h2").text().split(" ")[0] + ' ' + date.getDate());
 }
 $(initialize);
