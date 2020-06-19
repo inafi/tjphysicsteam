478fb0687d18d9157d9caba42d6ad864dd070c08
inafi <irfan.k.nafi@gmail.com>
2020-06-18 22:23:01 -0400

    Update pdf.py

--- Lectures/pdf.py
+++ Lectures/pdf.py
@@ -6,14 +6,14 @@ for x in os.walk(PATH):
     for y in glob.glob(os.path.join(x[0], '*.pdf')):
         result.append(y)
 
-
 #gs -o page-1-of-input-PDF.png -sDEVICE=pngalpha -dLastPage=1 input.pdf
 
 for i in result:
     # if "/PPP/" in i:
-    f = i.split(".pdf")[0]
     #     print(i)
+    f = i.split(".pdf")[0]
+    if " " in i:
+        os.system("mv " + i.replace(" ", "\ ") + " " + i.replace(" ", "_"))
+        f = i.split(".pdf")[0].replace(" ", "_")
     os.system("gs -o " + f + ".png" + " -sDEVICE=pngalpha -dLastPage=1 " + i)
-    # if " " in i:
-        # os.system("mv " + i.replace(" ", "\ ") + " " + i.replace(" ", "_"))
     
