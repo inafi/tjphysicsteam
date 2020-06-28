a2494c38679270ce78b15778a9d151101c936f64
inafi <irfan.k.nafi@gmail.com>
2020-06-28 15:42:57 -0400

    Update rankings.js

--- JS/rankings.js
+++ JS/rankings.js
@@ -15,41 +15,41 @@ function initialize() {
         }
     }, 20)
 
-    var req =
-        "https://docs.google.com/spreadsheets/d/e/2PACX-1vSzHEmL1GrN-9nTc4KXIoIxB1-Pv3bfb4OlZuaxuG8RuaSLVW9WLliYPyR_mPZaPDQ_7L0MwOjouxXM/pub?gid=0&single=true&output=csv";
+    var req = "https://sheets.googleapis.com/v4/spreadsheets/1vBfqcpmkNG3xDgoDCBVgLwaWlV4t88Ev8hlsyXMOPuo/?key=AIzaSyAjX2wnpSdfn5KkEvaTwXMkTqCXxRRIxm8&includeGridData=true";
 
     $.ajax({
         url: req,
         type: "get",
         async: false,
         success: function (data) {
-            var arr = Papa.parse(data).data;
+            var arr = data.sheets[0].data[0].rowData;
+            console.log(arr);
             var append = '';
             append += '<tr class="content t1">';
 
-            for (i = 0; i < arr[0].length; i++)
-                append += '<td>' + arr[0][i] + '</td>';
+            for (i = 0; i < arr[0].values.length; i++)
+                append += '<td>' + arr[0].values[i].formattedValue + '</td>';
 
             append += '</tr>';
             append += '<tr class="content t2">';
 
-            for (i = 0; i < arr[1].length; i++)
-                append += '<td>' + arr[1][i] + '</td>';
+            for (i = 0; i < arr[1].values.length; i++)
+                append += '<td>' + arr[1].values[i].formattedValue + '</td>';
 
             append += '</tr>'
             $("table.test").append(append);
 
             for (i = 2; i < arr.length; i++) {
                 append = '<tr class="content">';
-                for (k = 0; k < arr[0].length; k++) {
-                    append += "<td>" + arr[i][k] + "</td>";
+                for (k = 0; k < arr[0].values.length; k++) {
+                    append += "<td>" + arr[i].values[k].formattedValue + "</td>";
                 }
                 append += '</tr><tr class="space"></tr>'
                 $("table.student").append(append);
             }
 
             $("table.student").css("height", (arr.length - 2) * 6 + "vh");
-            $("td").css("width", "calc(100%/" + arr[0].length + ")")
+            $("td").css("width", "calc(100%/" + arr[0].values.length + ")")
         }
     });
 
