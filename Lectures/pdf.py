859336f7fc65075a7a373bc2d78d988fe1cc9065
inafi <irfan.k.nafi@gmail.com>
2020-09-12 16:34:42 -0400

    lectures

--- Lectures/pdf.py
+++ Lectures/pdf.py
@@ -1,7 +1,7 @@
 import glob
 import os
 result = []
-PATH = "/Users/nafi/Develop/GitHub/tjphysicsteam/Lectures/Tests/F=ma"
+PATH = "/Users/nafi/Develop/GitHub/tjphysicsteam/Lectures/C/2D_Kinematics"
 for x in os.walk(PATH):
     for y in glob.glob(os.path.join(x[0], '*.pdf')):
         result.append(y)
